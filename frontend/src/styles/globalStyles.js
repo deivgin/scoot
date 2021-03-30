3d3df59630438753a2266ed87b206204f8a1b873
Deividas <deividasgineitis123@gmail.com>
2021-03-30 16:59:48 +0300

    Media query theme obj

--- frontend/src/styles/globalStyles.js
+++ frontend/src/styles/globalStyles.js
@@ -1,12 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import theme from "./theme";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    margin: 0 15rem; //change with mediaqueries
+    margin: 0 0; //change with mediaqueries
     padding: 0;
     font-family: 'Roboto', sans-serif;
     font-size: 62.5%;
     background-color: #F2F7F4;
+
+    @media(min-width: ${theme.screenSize.laptopL}){
+      margin: 0 15rem;
+    }
   }
 
   a {
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -14,6 +14,15 @@ const theme = {
   margin: {
     header: "2rem",
   },
+  screenSize: {
+    mobileS: "320px",
+    mobileM: "375px",
+    mobileL: "425px",
+    tablet: "768px",
+    laptop: "1024px",
+    laptopL: "1440px",
+    desktop: "2560px",
+  },
 };
 
 export default theme;
