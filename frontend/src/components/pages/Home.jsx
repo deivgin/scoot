218b622ede64b5ecf08f92a821b7a1228443eedc
Deividas <deividasgineitis123@gmail.com>
2021-03-30 14:15:05 +0300

    File prep for work.

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,11 +1,21 @@
-import Header from "../layouts/Header";
-import HomePageSection from "../layouts/HomePageSection";
+import styled from "styled-components";
+import Header from "../modules/Header";
+import HomeCanvas from "../modules/HomeCanvas";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <HomePageSection />
+      <Container>
+        <HomeCanvas />
+      </Container>
     </>
   );
 }
+
+//Styles
+const Container = styled.section`
+  height: 70vh;
+  border: 0.1rem solid ${({ theme }) => theme.color.primaryLight};
+  margin-top: 3rem;
+`;
