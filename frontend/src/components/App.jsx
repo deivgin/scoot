430f7088c277521768019f3bb740308559442551
Deividas <deividasgineitis123@gmail.com>
2021-04-06 11:43:07 +0300

    Fix rendering of routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,20 +1,30 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Signin from "./pages/Signin";
 import Header from "./modules/Header/Header";
+import About from "./pages/About";
+import Shop from "./pages/Shop";
 
 export default function App() {
   return (
     <Router>
-      <Header />
-      <Switch>
-        <Route path="/">
-          <Home />
-        </Route>
-        <Route path="/signin">
-          <Signin />
-        </Route>
-      </Switch>
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/signin">
+            <Signin />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
