d391a57e9d86224573080bc2dac81d92e54db81b
Deividas <deividasgineitis123@gmail.com>
2021-05-01 17:35:57 +0300

    Basic model generation, controls and view

--- frontend/src/components/modules/Shop3D/Scene.jsx
+++ frontend/src/components/modules/Shop3D/Scene.jsx
@@ -1,24 +1,34 @@
 import { Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import Model from "./model";
-import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
+import {
+  ContactShadows,
+  Environment,
+  OrbitControls,
+  Html,
+  useProgress,
+} from "@react-three/drei";
+
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
 
 export default function Scene() {
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+    <Canvas shadows dpr={[1, 2]} camera={{ fov: 40, position: [0, 0, 2] }}>
       <ambientLight intensity={0.7} />
       <spotLight intensity={0.5} position={[10, 15, 10]} castShadow />
-      <Suspense fallback={null}>
-        <Model />
-
+      <Suspense fallback={<Loader />}>
+        <Model position={[0, -0.5, 0]} />
         <Environment preset="city" />
         <ContactShadows
           rotation-x={Math.PI / 2}
-          position={[0, -0.8, 0]}
-          opacity={0.25}
+          position={[0, -0.6, 0]}
+          opacity={0.5}
           width={10}
           height={10}
-          blur={1.5}
+          blur={0.5}
           far={0.8}
         />
       </Suspense>
